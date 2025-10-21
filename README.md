84395e1e3119d86a26fd1d37e696db782a9dc541
matthieu-27 <matthieu27@pm.me>
2025-10-21 10:35:55 +0200

    Code fixes and improvements for Naval Battle:
    - Added checks to ensure ships don't cross the board boundaries.
    - Converted coordinates to use numeric indices.
    - Improved board display with row and column labels.

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Bataille navalle
+# Bataille Navale
 
-Exercice de la bataille navale en programmation orienté objet python
+Un jeu de bataille navale implémenté en Python utilisant des classes.
+
+## Description
+
+Ce projet est une implémentation du jeu de bataille navale en utilisant la programmation orientée objet en Python. Le jeu permet de placer des bateaux sur un plateau et de les afficher.
+
+## Fonctionnalités
+
+- Initialisation d'un plateau de jeu de taille personnalisable.
+- Ajout de bateaux avec différentes longueurs et orientations.
+- Affichage du plateau avec des étiquettes pour les lignes et les colonnes.
+
+## Exécution
+
+Pour exécuter le jeu, lancez le fichier `ship_wars_oop.py`.
+
+```bash
+python ship_wars_oop.py
--- ship_wars_oop.py
+++ ship_wars_oop.py
@@ -1,46 +1,52 @@
 #!/usr/bin/env python
 #  -*- coding: utf-8 -*-
 
-
 class Game:
     def __init__(self, size: int):
         self.size = size
         self.board = [[" " for _ in range(size)] for _ in range(size)]
 
-
-class Coordinate:
+class Coordinates:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
 
-
 class Boat:
-    def __init__(self, length: int, orientation: int, position: Coordinate):
+    def __init__(self, length: int, orientation: int, x: int, y: int):
         self.length = length
         self.orientation = orientation
-        self.position = position
-
+        self.position = Coordinates(x, y)
 
 class Board:
     def __init__(self, game: Game):
         self.game = game
         N = self.game.size
         self.view = [[" " for _ in range(N + 1)] for _ in range(N + 1)]
-
+        self.boats = []  # type: ignore
 
     def show(self):
-        for row in self.view:
-            print(row)
-
+        # Add column labels
+        print("  " + " ".join(chr(65 + i) for i in range(self.game.size)))
+        for i, row in enumerate(self.view):
+            if i < self.game.size:
+                # Add row labels
+                print(chr(65 + i) + " " + " ".join(row[1:]))
 
     def add_boat(self, boat: Boat):
-        if boat.orientation == 0:
-            for x in range(boat.position.x, boat.length + boat.position.x):
-                self.view[x][boat.position.y] = "X"
-        else:
-            for y in range(boat.position.y, boat.length + boat.position.y):
-                self.view[boat.position.x][y] = "X"
+        if boat.orientation == 0:  # Horizontal
+            for x in range(boat.position.x, boat.position.x + boat.length):
+                if x < self.game.size:
+                    self.view[x][boat.position.y] = "X"
+            self.boats.append(boat)
+        else:  # Vertical
+            for y in range(boat.position.y, boat.position.y + boat.length):
+                if y < self.game.size:
+                    self.view[boat.position.x][y] = "X"
+            self.boats.append(boat)
 
+    def add_fleet(self, boats):
+        for boat in boats:
+            self.add_boat(boat)
 
 if __name__ == '__main__':
     size = 9
@@ -48,6 +54,10 @@ if __name__ == '__main__':
 
     view = Board(game)
 
-    aircraft = Boat(5, 1, Coordinate(1,2))
-    view.add_boat(aircraft)
+    aircraft = Boat(5, 0, 2, 2)
+    cruiser = Boat(4, 1, 1, 4)
+    destroyer = Boat(3, 1, 3, 5)
+    submarine = Boat(3, 0, 8, 5)
+    torpedo = Boat(2, 0, 5, 9)
+    view.add_fleet([aircraft, cruiser, destroyer, submarine, torpedo])
     view.show()
